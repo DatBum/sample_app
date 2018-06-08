96e63641e58486b66e36d62165e21a40b7ea6563
Dat Luong <datluong20@gmail.com>
2018-06-08 14:51:07 +0700

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by id: params[:id]
-    if @user.nil?
-      flash[:danger] = t "usernotfound"
-      redirect_to root_url
-    end
+    return unless @user.nil?
+    flash[:danger] = t "usernotfound"
+    redirect_to root_url
   end
 
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t "success"
       redirect_to @user
     else

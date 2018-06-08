180ec49231f03fae64f80c5c02656b6197982d23
Dat Luong <datluong20@gmail.com>
2018-06-08 14:51:07 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,26 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(new show create)
+  before_action :load_user, except: %i(new index create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
   def new
     @user = User.new
   end
 
-  def show
-    @user = User.find_by id: params[:id]
-    return unless @user.nil?
-    flash[:danger] = t "usernotfound"
-    redirect_to root_url
+  def show; end
+
+  def index
+    @users = User.paginate page: params[:page]
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "userdeleted"
+    else
+      flash[:warning] = t "usernotdeleted"
+    end
+    redirect_to users_url
   end
 
   def create
@@ -21,10 +34,43 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "pleaselogin"
+    redirect_to login_url
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return unless @user.nil?
+    flash[:danger] = t "usernotfound"
+    redirect_to root_url
   end
 end
